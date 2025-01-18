2f18a1f1d39646f15b90723c7b72cc65dc327969
vasanthcodes <vasanthsanku2719@gmail.com>
2025-01-18 21:28:58 +0530

    completed tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheitNum) {
+  return Math.round(((farenheitNum-32)*5/9)*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusNumber) {
+  return Math.round((((celsiusNumber*9)/5)+32)*10)/10;
 };
 
 // Do not edit below this line
