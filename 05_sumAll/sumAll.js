6bf0a0170dca2d399321ace144cec4db540e380b
vasanthcodes <vasanthsanku2719@gmail.com>
2025-01-18 19:57:53 +0530

    completed sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (num, end) {
+  var ini;
+    var mul;
+    let i;
+  if (num >0&& end> 0 && Number.isInteger(num )&&Number.isInteger(end) ){
+    
+    if (num > end) {
+      mul = num;
+      ini = end;
+    } else {
+      ini = num;
+      mul = end;
+    }
 
+    for (i = 0; i <= mul; i++) {
+      ini = ini + i;
+    }
+    return ini - 1;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
